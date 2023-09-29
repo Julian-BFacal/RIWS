dee07dafecf6265ad3be042e906450acf7968764
Julian-BFacal <julian.bfacal@udc.es>
2023-09-29 18:21:57 +0200

    Ejemplo para un libro especifico
    Solo filtrado por nombre, falta engadir outros modelos e revisar se realmente soup.select funciona ben

--- Crawler/crawler/crawler/spiders/example.py
+++ Crawler/crawler/crawler/spiders/example.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from crawler.items import Book
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from bs4 import BeautifulSoup
 
 class ExampleSpider(CrawlSpider):
     name = 'example'
     allowed_domains = ['casadellibro.com']
-    start_urls = ['https://www.casadellibro.com/']
+    start_urls = ['https://www.casadellibro.com/libro-la-chica-del-verano-novela/9788448038977/14166923/']
 
     custom_settings = {
                 'CLOSESPIDER_ITEMCOUNT' : 10,
@@ -15,13 +17,15 @@ class ExampleSpider(CrawlSpider):
     rules = (
         Rule(
             LinkExtractor(
-                allow=""),
+                allow="www.casadellibro.com/libro-"),
                 callback='parse_item',
-                follow=True
+                follow=False
             ),
         )
 
 
     def parse_item(self, response):
-        page = response.url
-        print(page)
+        book = Book()
+        soup = BeautifulSoup(response.body, 'lxml')
+        book['name'] = soup.select('h1', class_='text-h4 mb-2')[0].text.strip()
+        yield book
