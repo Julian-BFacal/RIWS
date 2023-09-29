96224d0463fc0edb4c609d324bcea983942a7993
MrBrenlla <bgbrenlla@gmail.com>
2023-09-29 16:16:18 +0200

    Spider creada

--- Crawler/crawler/crawler/spiders/example.py
+++ Crawler/crawler/crawler/spiders/example.py
@@ -1,11 +1,27 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
-
-class ExampleSpider(scrapy.Spider):
+class ExampleSpider(CrawlSpider):
     name = 'example'
     allowed_domains = ['example.com']
     start_urls = ['http://example.com/']
 
-    def parse(self, response):
-        pass
+    custom_settings = {
+                'CLOSESPIDER_ITEMCOUNT' : 10,
+                'DOWNLOAD_DELAY' : 0.5}
+
+    rules = (
+        Rule(
+            LinkExtractor(
+                allow=""),
+                callback='parse_item',
+                follow=True
+            ),
+        )
+
+
+    def parse_item(self, response):
+        page = response.url
+        print(page)
