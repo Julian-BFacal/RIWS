f1e140ad50d8d3fab8ef24703511fc7829097e7b
Julian-BFacal <julian.bfacal@udc.es>
2023-09-29 18:21:04 +0200

    Creado pipeline para enviar datos a elastic

--- Crawler/crawler/crawler/pipelines.py
+++ Crawler/crawler/crawler/pipelines.py
@@ -4,8 +4,27 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+from elasticsearch import Elasticsearch, helpers
+import json
 
 class CrawlerPipeline(object):
+    listItems = []
+
     def process_item(self, item, spider):
+        self.listItems.append(json.dumps(dict(item)))
         return item
+
+    def close_spider(self, spider):
+
+        es = Elasticsearch('http://localhost:9200')
+
+        actions = [
+            {
+                "_index": "book",
+                "_source": node
+            }
+            for node in self.listItems
+        ]
+        print(actions)
+
+        helpers.bulk(es, actions)
