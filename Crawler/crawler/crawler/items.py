906d80a3b051f644149fe10320380ae964da0c02
Julian-BFacal <julian.bfacal@udc.es>
2023-10-03 12:30:27 +0200

    Arreglados head

--- Crawler/crawler/crawler/items.py
+++ Crawler/crawler/crawler/items.py
@@ -11,7 +11,6 @@ from scrapy.item import Item, Field
 class Book(Item):
     # define the fields for your item here like:
     name = Field()
-<<<<<<< HEAD
     author = Field()
     editorial = Field()
     year = Field()
@@ -19,6 +18,4 @@ class Book(Item):
     language = Field()
     price = Field()
     pages = Field()
-=======
->>>>>>> 5690f2c9626b4b3a58afa1aa57f23178a08431a6
     pass
--- Crawler/crawler/crawler/spiders/example.py
+++ Crawler/crawler/crawler/spiders/example.py
@@ -4,11 +4,6 @@ from crawler.items import Book
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from bs4 import BeautifulSoup
-<<<<<<< HEAD
-
-mydict = ""
-=======
->>>>>>> 5690f2c9626b4b3a58afa1aa57f23178a08431a6
 
 class ExampleSpider(CrawlSpider):
     name = 'example'
@@ -27,25 +22,12 @@ class ExampleSpider(CrawlSpider):
                 callback='parse_item',
                 follow=False
             ),
-        Rule(
-            LinkExtractor(
-<<<<<<< HEAD
-                allow="www.casadellibro.com/libro-"),
-                callback='parse_item',
-                follow=False
-=======
-                allow=""),
-                callback='',
-                follow=True
->>>>>>> 5690f2c9626b4b3a58afa1aa57f23178a08431a6
-            ),
         )
 
     def parse_item(self, response):
         book = Book()
         soup = BeautifulSoup(response.body, 'lxml')
         book['name'] = soup.select('h1', class_='text-h4 mb-2')[0].text.strip()
-<<<<<<< HEAD
         book["author"] = soup.find('a', attrs={'class': 'text--darken-1 d-flex grey--text'})["data-autor-link"]
         for element in soup.find_all('div', attrs={'class': 'row text-body-2 py-1 no-gutters'}):
             value = element.get_text().split(":")
@@ -58,6 +40,4 @@ class ExampleSpider(CrawlSpider):
             if value[0] == "Idioma":
                  book["language"] = value[1]
         yield book
-=======
-        yield book
->>>>>>> 5690f2c9626b4b3a58afa1aa57f23178a08431a6
+
