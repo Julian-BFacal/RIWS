59ce0c1f8e96ec392d6a29e79985f338914dfffa
MrBrenlla <bgbrenlla@gmail.com>
2023-11-03 16:51:26 +0100

    Crawler listo

--- Crawler/crawler/crawler/spiders/example.py
+++ Crawler/crawler/crawler/spiders/example.py
@@ -13,9 +13,9 @@ class ExampleSpider(CrawlSpider):
     start_urls = ['https://www.casadellibro.com/']
 
     custom_settings = {
-                'CLOSESPIDER_ITEMCOUNT' : 1,
-                'CLOSESPIDER_PAGECOUNT' : 3,
-                'DOWNLOAD_DELAY' : 0}
+                'CLOSESPIDER_ITEMCOUNT' : 10,
+                'CLOSESPIDER_PAGECOUNT' : 300000,
+                'DOWNLOAD_DELAY' : 0.5}
 
     rules = (
         Rule(
@@ -45,7 +45,7 @@ class ExampleSpider(CrawlSpider):
         try:
             book['sinopsis']=soup.find_all("div",attrs={"class":"formated-text"})[0].get_text()
         except:
-            none=None
+            book["sinopsis"]=None
         aux=soup.find('a', attrs={'class': 'text--darken-1 d-flex grey--text'})
         if aux is not None:
             book['author'] = aux["data-autor-link"]
@@ -55,15 +55,24 @@ class ExampleSpider(CrawlSpider):
         aux=soup.find('div', attrs={'class': 'swiper-img-container'}).find('img')
         if aux is not None:
             book['portada'] =aux['src']
+        else:
+            book['portada']=None
+
+        book["pages"]=None
+        book["editorial"]=None
+        book["year"]=None
+        book["language"]=None
 
         for element in soup.find_all('div', attrs={'class': 'row text-body-2 py-1 no-gutters'}):
             value = element.get_text().split(":")
             if value[0] == "Nº de páginas":
                  book["pages"] = value[1]
+                 print(value[1])
             if value[0] == "Editorial":
                  book["editorial"] = value[1]
             if value[0] == "Año de edición":
                  book["year"] = value[1]
+                 print(value[1])
             if value[0] == "Idioma":
                  book["language"] = value[1]
 
